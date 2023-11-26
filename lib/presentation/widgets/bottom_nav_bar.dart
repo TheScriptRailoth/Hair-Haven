72ef87c66d3787750bcca9912cc9a0c332a8fbfc
Ashutosh <am3718440@gmail.com>
2023-11-26 13:14:24 +0530

    HAIR-HAVEN: Added Bottom Navigation Bar
    Signed-off-by: Ashutosh <am3718440@gmail.com>

--- lib/presentation/widgets/bottom_nav_bar.dart
+++ lib/presentation/widgets/bottom_nav_bar.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:hair_haven/core/theme/mycolors.dart';
 import 'package:hair_haven/presentation/pages/home_screen.dart';
 import 'package:hair_haven/presentation/pages/profile_screen.dart';
 class BottomNavBar extends StatelessWidget {
@@ -18,18 +20,19 @@ class BottomNavBar extends StatelessWidget {
           elevation: 0.0,
           height: 50.h,
           backgroundColor: Color(0xffEEEDED),
+          indicatorColor: MyColors.primaryColor.withOpacity(0.15),
           selectedIndex: controller.selectedIndex.value,
           onDestinationSelected: (index)=> controller.selectedIndex.value= index,
           destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: ''),
-            NavigationDestination(icon: Icon(Icons.search), label: ''),
-            NavigationDestination(icon: Icon(Icons.calendar_month), label: ''),
-            NavigationDestination(icon: Icon(Icons.notifications_active), label: ''),
-            NavigationDestination(icon: Icon(Icons.person), label: ''),
+            NavigationDestination(icon: controller.selectedIndex.value==0?Icon(CupertinoIcons.home, color: MyColors.primaryColor,):Icon(CupertinoIcons.home), label: '',),
+            NavigationDestination(icon: controller.selectedIndex.value==1?Icon(CupertinoIcons.search, color: MyColors.primaryColor,):Icon(CupertinoIcons.search), label: ''),
+            NavigationDestination(icon: controller.selectedIndex.value==2?Icon(CupertinoIcons.calendar,color: MyColors.primaryColor,):Icon(CupertinoIcons.calendar), label: ''),
+            NavigationDestination(icon: controller.selectedIndex.value==3?Icon(CupertinoIcons.bell,color: MyColors.primaryColor,):Icon(CupertinoIcons.bell), label: ''),
+            NavigationDestination(icon: controller.selectedIndex.value==4?Icon(CupertinoIcons.person,color: MyColors.primaryColor,):Icon(CupertinoIcons.person), label: ''),
           ],
         ),
       ),
-      body: controller.screens[controller.selectedIndex.value],
+      body: Obx(()=>controller.screens[controller.selectedIndex.value]),
     );
   }
 }
